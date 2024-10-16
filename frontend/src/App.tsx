53aaef9bd821234d36c19f25c40dd176770d50a5
martapla <martaplanella@hotmail.com>
2024-10-16 12:14:11 +0200

    folders

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import Button from '@mui/material/Button';
 
 function App() {
-  return <p>Bona tarda</p>;
+  return <Button variant="contained">Hello world</Button>;
 }
 
 export default App;
