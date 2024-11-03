067ef8f43306b61339740846c385167e00e3dcde
martapla <martaplanella@hotmail.com>
2024-11-03 01:47:35 +0100

    style

--- frontend/src/pages/home/LandingPage/Landing.tsx
+++ frontend/src/pages/home/LandingPage/Landing.tsx
@@ -7,12 +7,12 @@ import { Box, Typography, Button } from '@mui/material'
 const Landing = () => {
   return (
     <>
-     <Box
+    <Box
       display="flex"
       flexDirection="column"
-      alignItems="center"      // Centra todos los elementos dentro del Box
-      minHeight="100vh"        // Ocupa toda la altura de la pantalla
-      justifyContent="center"  // Centra verticalmente
+      alignItems="center"      
+      minHeight="100vh"        
+      justifyContent="center"  
     >
     <Typography
       variant="h1"
@@ -40,9 +40,9 @@ const Landing = () => {
         }}
       >
         Made to Measure Lingerie
-      </Typography>
-
-       <Box
+    </Typography>
+                            {/* Main Image */}
+    <Box
       component="img"
       src={vpink}
       alt="Descripción de la imagen"
@@ -57,33 +57,33 @@ const Landing = () => {
         opacity: 0.9
       }} />
 
-      <Typography
-        variant="h2"
-        sx={{
-          fontFamily: 'Mandali, sans-serif', 
-          fontSize: 60,
-          fontWeight: 200,                   
-          color: '#444444',
-          textAlign: 'center',
-          mt:8,
-          mb:3
-        }}
-      >
-        Brand Philosophy
-      </Typography>
+    <Typography
+      variant="h2"
+      sx={{
+        fontFamily: 'Mandali, sans-serif', 
+        fontSize: 60,
+        fontWeight: 200,                   
+        color: '#444444',
+        textAlign: 'center',
+        mt:8,
+        mb:3
+      }}
+    >
+      Brand Philosophy
+    </Typography>
 
-      <Typography
-        variant="h2"
-        sx={{
-          fontFamily: 'Mandali, sans-serif', 
-          fontSize: 20,
-          fontWeight: 300,                   
-          color:'#444444',
-          textAlign: 'center',
-          mb: 10,
-          width:'80%'
-        }}
-      >
+    <Typography
+      variant="h2"
+      sx={{
+        fontFamily: 'Mandali, sans-serif', 
+        fontSize: 20,
+        fontWeight: 300,                   
+        color:'#444444',
+        textAlign: 'center',
+        mb: 10,
+        width:'80%'
+      }}
+    >
         In the heart of our brand, there’s a gentle promise: to craft 
         lingerie that whispers to the soul, as unique as each body it adorns. 
         We believe in softness and strength, in the beauty of curves, edges, 
@@ -92,100 +92,108 @@ const Landing = () => {
         made to treasure, our lingerie honors the earth, leaving little behind but elegance. 
         Here, you’re seen, cherished, embraced – wrapped in something made just for you, 
         to wear like a second, truest skin.
-      </Typography>
+    </Typography>
 
-      <Button
+    <Button
       variant="contained"
       onClick={() => alert('see-collection')}
       sx={{
         backgroundColor: 'white',       
-        color: '#e00845',               
-        border: '1px solid #e00845',    
+        color: '#e57390',               
+        border: '1px solid #e57390',    
         padding: '20px',          
         fontSize: '20px',               
         borderRadius: '50%',             
         fontFamily: 'Mandali, sans-serif', 
         fontWeight: 300,
         textTransform: 'none',          
-        boxShadow: '0px 6px 0px #e00845', 
+        boxShadow: '-10px 0px 0px #e57390', 
         '&:hover': {
-          color:  '#e57390',                // Color de texto rosa más oscuro al pasar el mouse
-          borderColor:  '#e57390',          // Cambia el borde a un tono rosa más oscuro
-          boxShadow: '0px 8px 0px #e57390',   // Cambia la sombra a un tono rosa más oscuro
+          backgroundColor: 'white', 
+          color:  '#e57390',               
+          borderColor:  '#e57390',          
+          boxShadow: '10px 0px 0px #e57390',   
         },
         mb: 14,                          
       }}
     >
-      See Collection
-    </Button>
-
-    <Box
-      component="img"
-      src={inside}
-      alt="Descripción de la imagen"
-      sx={{
-        width: '60%',
-        height: 'auto',
-        margin: '0 auto',    
-        borderRadius: 2, 
-        border: '10px solid white',  
-        boxShadow: 3,
-        //objectFit: 'contain',
-        
-      }} />
+      Collection
+  </Button>
 
 
-    <Typography
+  <Box
+    sx={{
+      flex: 1,
+      textAlign:'center',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems:'center',
+      px: 3,
+    }}
+  >
+      <Typography
         variant="h2"
         sx={{
-          fontFamily: 'Mandali, sans-serif', 
+          fontFamily: 'Mandali, sans-serif',
           fontSize: 60,
-          fontWeight: 200,                   
+          fontWeight: 200,
           color: '#444444',
-          textAlign: 'center',
-          mt:8,
-          mb:3
+          mb: 3
         }}
       >
         Our Studio
       </Typography>
 
       <Typography
-        variant="h2"
+        variant="body1"
         sx={{
-          fontFamily: 'Mandali, sans-serif', 
+          fontFamily: 'Mandali, sans-serif',
           fontSize: 20,
-          fontWeight: 300,                   
+          fontWeight: 300,
           color: '#444444',
           mb: 4,
-          width:'80%'
+          width: '80%'
         }}
       >
-        We specialize in crafting made-to-measure lingerie that embodies elegance 
-        and individuality. Each piece is thoughtfully designed to celebrate the unique 
-        beauty of every body. With a focus on quality craftsmanship, we take the time to 
-        understand your personal style and preferences, ensuring that every detail 
-        resonates with you.
+        We specialize in crafting made-to-measure lingerie that embodies elegance and individuality. Each piece is thoughtfully designed 
+        to celebrate the unique beauty of every body. With a focus on quality craftsmanship, we take the time to understand your personal 
+        style and preferences.
       </Typography>
+
       <Typography
-        variant="h2"
+        variant="body1"
         sx={{
-          fontFamily: 'Mandali, sans-serif', 
+          fontFamily: 'Mandali, sans-serif',
           fontSize: 20,
-          fontWeight: 300,                   
+          fontWeight: 300,
           color: '#444444',
-          mb: 10,
-          width:'80%'
+          mb: 8,
+          width: '80%'
         }}
       >
-        Our dedicated team of artisans meticulously handcrafts each item, 
-        combining modern techniques with timeless artistry. By prioritizing sustainability, 
-        we create pieces that are not only stunning but also mindful of our impact on the world. 
+        Our dedicated team of artisans meticulously handcrafts each item, combining modern techniques with timeless artistry. 
+        By prioritizing sustainability, we create pieces that are not only stunning but also mindful of our impact on the world.
       </Typography>
 
-
-      </Box>
-      </>
+      <Box
+        component="img"
+        src={inside}
+        alt="Descripción de la imagen"
+        sx={{
+          flex: 1,
+          width: '60%',
+          height: 'auto',
+          margin: { xs: '20px auto', md: '0 auto' },
+          borderRadius: 2,
+          border: '10px solid white',
+          boxShadow: 3,
+          maxWidth: '100%',
+        }}
+      />
+    </Box>
+  </Box>
+    
+  </>
       
   )
 }
